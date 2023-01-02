0df048096796f1fc2a4e174535ff9b24870f3bfc
Hani Nurchairil <haninurchairil@MacBook-Pro-Hani.local>
2023-01-02 18:51:22 +0700

    tambah jawaban

---
+++ tugas2/jawab-tugas2.py
@@ -0,0 +1,24 @@
+# Jawab
+separator="========================"
+separator2="---"
+
+katadepan="Ayo Semangat"
+katatengah="TAKLIM"
+katabelakang="jangan loyo"
+
+# Soal 1
+print("Soal 1\n",separator2)
+print("jumlah char katatengah:",len(katatengah))
+print("jumlah char katatengah:",len(katabelakang))
+print("ada, perbedaan ada pada 'katabelakang' dimana pada variable katabelakang terdapat character spasi ' ' sehingga jumlah total char adalah 11")
+print(separator)
+
+# Soal 2
+print("Soal 2\n",separator2)
+print(katadepan,katatengah,katabelakang,"!!")
+print(separator)
+
+# Soal 3
+print("Soal 3\n",separator2)
+print(katadepan.capitalize(),katatengah.lower(),katabelakang,"!!")
+print(separator)
