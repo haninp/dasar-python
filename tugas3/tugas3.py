02ec84a4d5b75054c9d301607b6d8bccfebe5aa2
IJAB21 <aakuijab666@gmail.com>
2023-01-05 19:07:17 +0700

    tugas 3

---
+++ tugas3/tugas3.py
@@ -0,0 +1,22 @@
+x = "88"
+y = 88
+
+x = "5"
+print(type(int(x)))
+y=5
+print(type(str(y)))
+"jawaban soal 3"
+'int'
+'str'
+
+"jawaban soal 2"
+x = "5"
+print(type(int(x)))
+y = 5
+print(type(str(y)))
+
+"jawaban soal 3"
+"10"
+10
+yg sukses adalah 10 
+alasanya karena y tidak bisa ditambah x
