cf3240ab4521f74ae6cccff82528299cce4610f2
IJAB21 <aakuijab666@gmail.com>
2023-01-10 13:07:04 +0700

    ba tugas 3 nih

--- tugas2/tugas2.py
+++ tugas2/tugas2.py
@@ -1,3 +1,10 @@
 katadepan="Ayo Semangat"
 katatengah="TAKLIM"
 katabelakang="jangan loyo"
+
+
+
+
+katadepan="Ayo Semangat "
+katatengah="TAKLIM"
+katabelakang="TAKLIM"
--- tugas3/tugas3.py
+++ tugas3/tugas3.py
@@ -1,2 +1,28 @@
-x = "88"
-y = 88
+# ijabhandsome = "ganteng"
+# print(ijabhandsome)
+# print(ijabhandsome.upper())
+# # print(ijabhandsome.title(),"intan 2",ijabhandsome,ijabhandsome.title())
+# angka= 25
+# nomer= 30
+# kata= nomer - angka
+# print(kata)
+# print(kata)
+# print(kata - 50)
+# wahid=300
+# print(wahid+angka+50)
+# print(type(angka))
+# print(type(nomer))
+# print(nomer-angka)
+# isnani=200
+# print(isnani+wahid+nomer)
+# print(isnani+wahid+nomer)
+
+x = "88" #(kutip itu string)
+y = 88 #(intejer)
+print(type(x))
+print(type(y))
+print(type(str(y)))
+print(type(int(x)))
+z= int(x) + y
+print(z)
+
