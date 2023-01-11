3c1fb2b522e77f8938d1f5f6244ec0828c6c0cd7
IJAB21 <aakuijab666@gmail.com>
2023-01-11 19:41:41 +0700

    tugas 4 kelar boss

---
+++ tugas4/tugas4.py
@@ -0,0 +1,53 @@
+# CONTOH !
+# def tambah(a,b):
+#     c=a+b
+#     return c
+
+#jawaban soal 1 -penjumlahan
+def tambah (a,b):
+    c=a+b
+    return c
+a = 50
+b = 25 
+print(tambah(50,25))
+#jawaban soal 1 -pengurangan
+def kurang (a,b):
+    c=a-b
+    return c
+a = 100
+b = 30
+print(kurang(100,30))
+#jawaban soal 1 -perkalian 
+def kali (a,b):
+    c=a*b
+    return c
+a = 45
+b = 20
+print(kali(45,20))
+#jawaban no 1 pembagian 
+def bagi (a,b):
+    c=a//b
+    return c
+a = 60
+b = 5 
+print(bagi(60,5))
+
+## jawaban soal 2 
+# hasil penjumlahan = 75
+# hasil pengurangan = 70
+# hasil perkalian = 900
+# hasil pembagian = 12 
+
+## jawaban soal 3
+# x = 912 
+def tambah(a,b):
+    x=a+b
+    return x
+a=900
+b=12
+print(tambah(900,12))
+
+
+
+
+
