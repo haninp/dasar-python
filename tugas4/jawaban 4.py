34dbfb1e16f53551f09233de7c0a7e8c6ac298ca
Rizky Maulana Ishaq <114470649+Ragaramore@users.noreply.github.com>
2023-01-12 18:44:15 +0700

    Update jawaban 4.py

--- tugas4/jawaban 4.py
+++ tugas4/jawaban 4.py
@@ -2,9 +2,11 @@
 def hitung (a,b):
     c=a+b #Penjumlahan#
     e=a-b #Pengurangan#
-    r=a*b #Pengurangan#
-    s=a/b #Pengurangan#
+    r=a*b #Perkalian#
+    s=a/b #Pembagian#
     x=r+s #jumlahkan perkalian dan pembagian#
     return c,e,r,s,x
 
 print(hitung(9,5))
+
+
