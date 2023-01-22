24262314b4e4b7bb19b3d1b37889bb199a5672e1
Tuharea24 <ubaidbinihsan20@gmail.com>
2023-01-23 03:02:45 +0700

    bagus dah

---
+++ tugas1/tugas1baru.py
@@ -0,0 +1,16 @@
+bos="======"
+bos2="------"
+print("soal 1\n",bos2)
+print("muhammad iksan tuharea")
+print("14 desember 1993")
+print(bos)
+print("soal 2\n",bos2)
+print("Muhammad\nIksan\nTuharea")
+print(bos)
+print("soal 3\n",bos2)
+kalimat="Masjid an-Nur limo Depok"
+print(kalimat.title())
+print(kalimat.upper())
+print(kalimat.lower())
+print(bos)
+
